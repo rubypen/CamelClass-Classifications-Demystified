f7cfa11e1ee3ad4b335c072fe5dcf4ffebb1efbc
Neha Naveen <njn43@cornell.edu>
2024-10-23 16:49:43 -0400

    Added Euclidian Distance tests for 2D and 3D points.

--- test/test_GroupProject.ml
+++ test/test_GroupProject.ml
@@ -3,10 +3,23 @@ open GroupProject.Point
 open GroupProject.Csvreader
 
 let test_euclidean_distance _ =
-  let p1 = Point1D.create 3.0 in
-  let p2 = Point1D.create 7.5 in
+  (* 1D tests*)
+  let p1 = Point1D.create [ 3.0 ] in
+  let p2 = Point1D.create [ 7.5 ] in
   let dist = Point1D.euclidean_distance p1 p2 in
-  assert_equal dist 4.5 ~printer:string_of_float
+  assert_equal dist 4.5 ~printer:string_of_float;
+
+  (* 2D test *)
+  let p1 = Point2D.create [ 1.0; 2.0 ] in
+  let p2 = Point2D.create [ 4.0; 6.0 ] in
+  let dist = Point2D.euclidean_distance p1 p2 in
+  assert_equal dist 5.0 ~printer:string_of_float;
+
+  (* 3D test *)
+  let p1 = Point3D.create [ 6.0; 15.0; 7.0 ] in
+  let p2 = Point3D.create [ 2.0; 3.0; 1.0 ] in
+  let dist = Point3D.euclidean_distance p1 p2 in
+  assert_equal dist 14.0 ~printer:string_of_float
 
 let test_read_points _ =
   let points = CsvReaderImpl.read_points "../data/test_data.csv" in
@@ -14,8 +27,8 @@ let test_read_points _ =
 
 let rec test_cases =
   [
-    ("Test Point1D" >:: fun _ -> test_euclidean_distance ());
+    ("Test Points" >:: fun _ -> test_euclidean_distance ());
     ("Test CsvReader" >:: fun _ -> test_read_points ());
   ]
 
-let () = run_test_tt_main ("priority queue tests" >::: test_cases)
+let () = run_test_tt_main ("Point Tests" >::: test_cases)
