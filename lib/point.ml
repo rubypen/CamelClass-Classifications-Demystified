95dc6be1db7e3dcc6ed9b8ca63489fc73a4c55e0
Ruby <rsp227@cornell.edu>
2024-10-28 16:38:06 -0400

    Add to_string function to Point Implementations

--- lib/point.ml
+++ lib/point.ml
@@ -9,13 +9,26 @@ module type Point = sig
   val get_coordinate : t -> float list
   (** [get_coordinate p] is the coordinate of the point [p]. *)
 
+  val to_string : t -> string
+  (** [to_string p] is the string representation of [p] *)
+
   val euclidean_distance : t -> t -> float
   (** [euclidean_distance p1 p2] is the Euclidean distance between points [p1]
       and [p2]. *)
 end
 
+(** Module containing helper methods for the Point implementation modules*)
+module PointHelpers = struct
+  let no_decimal_places f = int_of_float (f *. 10.) mod 10 = 0
+
+  let str f =
+    if no_decimal_places f then string_of_float f ^ "0" else string_of_float f
+end
+
 (* Implementation for 1 Dimensional Point *)
 module Point1D : Point = struct
+  include PointHelpers
+
   type t = float
 
   let create p =
@@ -25,9 +38,12 @@ module Point1D : Point = struct
 
   let get_coordinate (p : t) = [ p ]
   let euclidean_distance (p1 : t) (p2 : t) : float = abs_float (p1 -. p2)
+  let to_string p = Printf.sprintf "(%s)" (str p)
 end
 
 module Point2D : Point = struct
+  include PointHelpers
+
   type t = float * float
 
   let create lst =
@@ -41,9 +57,13 @@ module Point2D : Point = struct
     let x1, y1 = p1 in
     let x2, y2 = p2 in
     sqrt (((x2 -. x1) ** 2.0) +. ((y2 -. y1) ** 2.0))
+
+  let to_string (x, y) = Printf.sprintf "(%s, %s)" (str x) (str y)
 end
 
 module Point3D : Point = struct
+  include PointHelpers
+
   type t = float * float * float
 
   let create lst =
@@ -57,4 +77,7 @@ module Point3D : Point = struct
     let x1, y1, z1 = p1 in
     let x2, y2, z2 = p2 in
     sqrt (((x2 -. x1) ** 2.0) +. ((y2 -. y1) ** 2.0) +. ((z2 -. z1) ** 2.0))
+
+  let to_string (x, y, z) =
+    Printf.sprintf "(%s, %s, %s)" (str x) (str y) (str z)
 end
