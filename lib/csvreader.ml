ce747002cb6038b8855403194f535915bc3b9af8
vb348 <17971+vb348@users.noreply.github.coecis.cornell.edu>
2024-11-07 03:43:35 -0500

    Updated csv module for new abstract type

--- lib/csvreader.ml
+++ lib/csvreader.ml
@@ -3,53 +3,21 @@ open Csv
 
 (* Module type signature for CsvReader *)
 module type CsvReader = sig
-  val read_points_1d : string -> Point1D.t list
-  (** [read_points_1d str] creates a list of points from csv file with a name
-      [str]. Requires: [lst] is a name of csv file. *)
-
-  val read_points_2d : string -> Point2D.t list
-  (** [read_points_2d str] creates a list of points from csv file with a name
-      [str]. Requires: [lst] is a name of csv file. *)
-
-  val read_points_3d : string -> Point3D.t list
-  (** [read_points_3d str] creates a list of points from csv file with a name
-      [str]. Requires: [lst] is a name of csv file. *)
+  val read_points : int -> string -> Point.t list
+  (** [read_points n str] creates a list of points with [n] coordinates each
+      from csv file with a name [str]. Requires: [lst] is a name of csv file. *)
 end
 
 module CsvReaderImpl : CsvReader = struct
-  let read_points_1d (file_path : string) : Point1D.t list =
+  let read_points n (file_path : string) : Point.t list =
     let csv = Csv.load file_path in
     List.map
       (fun row ->
-        match row with
-        | [ coordinate_as_str ] ->
-            let coord = float_of_string coordinate_as_str in
-            Point1D.create [ coord ]
-        | _ -> failwith "Invalid CSV format")
-      csv
-
-  let read_points_2d (file_path : string) : Point2D.t list =
-    let csv = Csv.load file_path in
-    List.map
-      (fun row ->
-        match row with
-        | [ x_str; y_str ] ->
-            let x = float_of_string x_str in
-            let y = float_of_string y_str in
-            Point2D.create [ x; y ]
-        | _ -> failwith "Invalid CSV format")
-      csv
-
-  let read_points_3d (file_path : string) : Point3D.t list =
-    let csv = Csv.load file_path in
-    List.map
-      (fun row ->
-        match row with
-        | [ x_str; y_str; z_str ] ->
-            let x = float_of_string x_str in
-            let y = float_of_string y_str in
-            let z = float_of_string z_str in
-            Point3D.create [ x; y; z ]
-        | _ -> failwith "Invalid CSV format")
+        let lst = ref [] in
+        for i = 0 to List.length row - 1 do
+          lst :=
+            float_of_string (List.nth row (List.length row - 1 - i)) :: !lst
+        done;
+        Point.create n !lst)
       csv
 end
