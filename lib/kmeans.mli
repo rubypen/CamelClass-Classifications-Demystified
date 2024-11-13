013371206dff61977e2cefb2a82c09b5ad7be243
Neha Naveen <njn43@cornell.edu>
2024-11-13 16:40:29 -0500

    Added running kmeans, variation, and finding best k-value/clustering documentation.

--- lib/kmeans.mli
+++ lib/kmeans.mli
@@ -16,3 +16,23 @@ val update_centroids : (Point.t * Point.t list) list -> Point.t list
 val has_converged : Point.t list -> Point.t list -> float -> bool
 (** [has_converged old_clusters new_clusters threshold] checks if the centroids
     have changed position by less than threshold. *)
+
+val run_range_kmeans : Point.t list -> Point.t list list
+(** [run_range_kmeans points] runs kmeans on [points] for a range of k values
+    from 1-10. *)
+
+val run_custom_kmeans : int -> Point.t list -> Point.t list
+(** [run_custom_kmeans k points] runs kmeans on [points] for a custom k value
+    [k]. *)
+
+val total_variation : Point.t list -> Point.t list -> float
+(** [total_variation points centroids] calculates the total varation from
+    [points] to [centroids]. *)
+
+val find_best_set : Point.t list list -> Point.t list -> Point.t list
+(** [find_best_set clusters_sets points] is the set of centroids in
+    [cluster_sets] with the least total variation from [points]. *)
+
+val find_best_k : Point.t list list -> Point.t list -> int
+(** [find_best_k clusters_sets points] is the value of k that produces the least
+    variation from [points]. *)
