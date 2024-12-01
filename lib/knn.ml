6c04f1d02eaa4ecaff79ee8018eb964cc4d74d3f
Neha Naveen <njn43@cornell.edu>
2024-12-01 01:52:46 -0500

    Added debugging statements.

--- lib/knn.ml
+++ lib/knn.ml
@@ -14,15 +14,40 @@ let rec take n lst =
   | x :: xs -> x :: take (n - 1) xs
 
 let k_nearest_neighbors k point points_list =
-  if k <= 0 then invalid_arg "k must be positive"
-  else if points_list = [] then invalid_arg "No labeled points provided"
+  if k <= 0 then invalid_arg "k\n   must be positive"
+  else if points_list = [] then invalid_arg "No labeled\n   points provided"
   else
     let sorted_points = sort_by_distance point points_list in
     take k sorted_points
 
+(* let classify k point points_list = let neighbors = k_nearest_neighbors k
+   point points_list in let labels = List.map snd neighbors in let counts =
+   List.fold_left (fun acc label -> let count = try List.assoc label acc with
+   Not_found -> 0 in (label, count + 1) :: List.remove_assoc label acc) []
+   labels in List.fold_left (fun (best_label, best_count) (label, count) -> if
+   count > best_count then (label, count) else (best_label, best_count)) ("", 0)
+   counts |> fst *)
+
+(* let classify k point points_list = let neighbors = k_nearest_neighbors k
+   point points_list in print_endline "Neighbors used for classification:";
+   List.iter (fun (_, label) -> print_endline label) neighbors;
+
+   let labels = List.map snd neighbors in let counts = List.fold_left (fun acc
+   label -> let count = try List.assoc label acc with Not_found -> 0 in (label,
+   count + 1) :: List.remove_assoc label acc) [] labels in
+
+   print_endline "Label counts:"; List.iter (fun (label, count) -> Printf.printf
+   "%s: %d\n" label count) counts;
+
+   let best_label, _ = List.fold_left (fun (best_label, best_count) (label,
+   count) -> if count > best_count then (label, count) else (best_label,
+   best_count)) ("", 0) counts in best_label *)
+
 let classify k point points_list =
   let neighbors = k_nearest_neighbors k point points_list in
   let labels = List.map snd neighbors in
+
+  (* Count occurrences of each label *)
   let counts =
     List.fold_left
       (fun acc label ->
@@ -30,8 +55,15 @@ let classify k point points_list =
         (label, count + 1) :: List.remove_assoc label acc)
       [] labels
   in
-  List.fold_left
-    (fun (best_label, best_count) (label, count) ->
-      if count > best_count then (label, count) else (best_label, best_count))
-    ("", 0) counts
-  |> fst
+
+  (* Sort counts by count (descending), break ties lexicographically *)
+  let counts =
+    List.sort
+      (fun (label1, count1) (label2, count2) ->
+        if count1 = count2 then String.compare label1 label2
+        else compare count2 count1)
+      counts
+  in
+
+  let best_label, _ = List.hd counts in
+  best_label
